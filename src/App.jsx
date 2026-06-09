df8a4a39e10b11e1106b50fae37765928ed11e19
01TUSHAR <01tusharpal@gmail.com>
2026-06-09 11:59:00 +0530

    deployemnt changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import PollDetail from "./pages/PollDetail"
 import MyPolls from "./pages/MyPolls"
@@ -6,13 +6,13 @@ import MyPolls from "./pages/MyPolls"
 
 function App() {
   return (
-    <BrowserRouter>
+   
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/poll/:pollId" element={<PollDetail />} />
         <Route path="/my-polls" element={<MyPolls />} />
       </Routes>
-    </BrowserRouter>
+    
   )
 }
 
